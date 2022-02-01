701b86f686cde08b4490b26fb90a33cbfd9197ca
Gali1432 <galiflores1432@gmail.com>
2022-02-01 14:51:36 -0700

    first commit

---
+++ CSC130_HangMan-main/main.py
@@ -0,0 +1,7 @@
+listOf4Letter = {"Hard", "Ship", "Game", ""  }
+print(listOf4Letter)
+
+
+
+listOf5Letter = {"Apple","Alone","Beach","Beams","Clock","China","Cream", "Dream","Drive", "Earth","Enemy","Error","Final","Force","Glass","Guide","Heart","Horse","Image","Index","Japan","Judge","Knife","Level","Light","Major","Metal","Music","Night","North","Offer","Order","Peace","Price","Peach","Queen","Radio","River","Scene","Shock","Space","Sugar","Theme","Trust","Union","Unity","Value","Video","Waste","Watch","Youth","Zebras"}
+print(listOf5Letter)
